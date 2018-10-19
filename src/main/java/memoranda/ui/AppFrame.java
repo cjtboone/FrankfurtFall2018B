ef704fed0658160027df7a61ebd177e996cbf8cd
Paul Traxler <ptraxler@Pauls-MacBook-Pro.local>
2018-10-19 02:24:09 -0500

    Reverting addition of wrong Task fix

--- src/main/java/memoranda/ui/AppFrame.java
+++ src/main/java/memoranda/ui/AppFrame.java
@@ -681,8 +681,11 @@ public class AppFrame extends JFrame {
     }
 
     protected void processWindowEvent(WindowEvent e) {
-        if (e.getID() == WindowEvent.WINDOW_CLOSING) {
+    	if (e.getID() == WindowEvent.WINDOW_CLOSING) {
+            if (Configuration.get("ON_CLOSE").equals("exit"))
                 doExit();
+            else
+                doMinimize();
         }
         else if ((e.getID() == WindowEvent.WINDOW_ICONIFIED)) {
             super.processWindowEvent(new WindowEvent(this,
